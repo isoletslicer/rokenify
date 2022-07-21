91e79ed3f4e01cb23f774558f6882adf2fc8fdc9
ryrulobo <ryru.lobo@gmail.com>
2022-07-21 23:23:52 +0700

    menambahkan fitur update tabel

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// Function untuk counter waktu
 var c = 0;
 var t;
 var timer_is_on = 0;
 
 function timedCount() {
   document.getElementById("txt").value = c;
-  c = c + 1;
+  c += 1;
   t = setTimeout("timedCount()", 1000);
 }
 
@@ -15,10 +16,22 @@ function doTimer() {
   }
 }
 
+let data = [];
+
 function stopCount() {
   clearTimeout(t);
   timer_is_on = 0;
-  document.getElementById("txt").value = c;
+  document.getElementById("txt").value = c - 1;
+
+  data.push(c - 1);
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i]) {
+      let sum = data.reduce((partialSum, a) => partialSum + a, 0);
+      document.getElementById(`time-${i}`).innerText = data[i];
+      document.getElementById("avg").innerText = (sum / data.length).toFixed(2);
+    }
+  }
 }
 
 function resetCount() {
